f0adf06a862cf0e00e3aac69ece44a835c54fd22
Violett <52976585+lastmarsch@users.noreply.github.com>
2019-07-29 15:22:16 +0600

    Update main.cpp

--- Serverless-Chat/main.cpp
+++ Serverless-Chat/main.cpp
@@ -5,6 +5,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    
+    QSettings *settings = new QSettings("settings.ini",QSettings::IniFormat);
+    settings->setValue("settings/port",1252);
+    settings->setValue("settings/IP", "127.0.0.1");
+    settings->sync();
+    
     w.setWindowTitle("Serverless-Chat");
     w.show();
 
